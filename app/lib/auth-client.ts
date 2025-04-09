d6750a18fc16bc429a94cac19086a1f557f27f31
CharleeWa <18888351756@163.com>
2025-04-09 11:48:44 +0800

    fix(auth): initialize auth client only once

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,8 +1,14 @@
 import { createAuthClient } from 'better-auth/vue'
 
 export const authClient = createAuthClient({
-  /** the base url of the server (optional if you're using the same domain) */
-  baseURL: process.env.BETTER_AUTH_URL
+  // your config
 })
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const {
+  signIn,
+  signOut,
+  signUp,
+  useSession,
+  forgetPassword,
+  resetPassword
+} = authClient
