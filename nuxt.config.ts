090b045454bb25c9523a9f4acd34a84a1e2bf645
CharleeWa <18888351756@163.com>
2025-04-09 13:12:55 +0800

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,8 @@ export default defineNuxtConfig({
         'clsx',
         'class-variance-authority',
         'reka-ui',
-        'tailwind-merge'
+        'tailwind-merge',
+        'better-auth/vue'
       ]
     }
   },
