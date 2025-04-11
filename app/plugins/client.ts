d5fcf46d362c10bb6fe3ff33030d66a6c003cf20
CharleeWa <18888351756@163.com>
2025-04-11 15:09:36 +0800

    fix(netlify): decode Base64 encoded request body

--- app/plugins/client.ts
+++ app/plugins/client.ts
@@ -3,7 +3,16 @@ import type { AppRouter } from '~~/server/trpc/routers'
 
 export default defineNuxtPlugin(() => {
   const trpc = createTRPCNuxtClient<AppRouter>({
-    links: [httpBatchLink({ url: '/api/trpc' })]
+    links: [
+      httpBatchLink({
+        url: '/api/trpc',
+        headers() {
+          return {
+            'Content-Type': 'application/json'
+          }
+        }
+      })
+    ]
   })
 
   return {
